b7e77a75162ae53fbc586782bbc024a6c8e4cced
chicobentojr <francisco.bento.jr@hotmail.com>
2015-02-19 15:12:53 -0300

    Update menuAdm.php

--- menuAdm.php
+++ menuAdm.php
@@ -60,7 +60,7 @@ if (isset($_GET['logout'])){
                     <a class="item" href="GerenciarAtividades.aspx">Atividades </a>
                     <a class="item" href="GerenciarQuestoes.aspx">Questões </a>
                     <a class="item" href="GerenciarUsuarios.aspx">Usuários </a>
-                    <a class="item" href="/Gerenciar/Mensagens.php">Mensagens </a>
+                    <a class="item" href="/gerenciar/mensagens.php">Mensagens </a>
                 </div>
             </div>
             <div class="title">
